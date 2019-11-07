fafb3cc3e9565c5efdd8c50925292aad2cecc705
unknown <Lakshmiroyal.1997@gmail.com>
2019-11-07 14:48:13 +0530

    C#

---
+++ Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.WriteLine("enter the number");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = sum * 10 + r;
+                n = n / 10;
+            }
+            if (temp == sum)
+
+                Console.Write("number is palindrome");
+
+            else
+
+                Console.Write("number is not palindrome");
+        }
+        
+    }
+}
